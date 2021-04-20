b8df1a727e1806523f097d4035e7f2cf7c2d16e5
Maycon Carlete <maycon.carlete@hotmail.com>
2021-04-19 21:28:06 -0300

    test: ensure Bowling getFrameControl return length of frame

--- tests/bowling.test.ts
+++ tests/bowling.test.ts
@@ -1,10 +1,22 @@
 import { Bowling } from "../src/bowling"
 import { Frame } from "../src/models/frame"
+type SutTypes = {
+    sut:Bowling
+}
+const frame: Frame[] = [new Frame(1,2), new Frame(3,4)]
+
+const makeSut = ():SutTypes => {
+    const sut: Bowling = new Bowling(frame)
+    return {sut}
+}
 
 describe('Bowling class', () => {
     test('Should each frame has two rolls', () => {
-        const frame: Frame[] = [new Frame(1,2), new Frame(3,4)]
-        const sut: Bowling = new Bowling(frame)
-        expect(sut.frame).toEqual(frame)
+        const {sut} = makeSut()
+        expect(sut.getFrame).toEqual([new Frame(1,2), new Frame(3,4)])
+    })
+    test('Should frameControl is the length of frame', () => {
+        const {sut} = makeSut()
+        expect(sut.getFrameControl).toBe(frame.length)
     })
 })
