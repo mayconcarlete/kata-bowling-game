b87a2c7b73f7212f00f6f1be55c1041688c05efe
Maycon Carlete <maycon.carlete@gmail.com>
2021-04-24 20:15:23 -0300

    test: ensure firstPlay will be filled when roll is called for the first time

--- tests/frame.test.ts
+++ tests/frame.test.ts
@@ -6,6 +6,12 @@ describe('Frame class', () => {
         expect(sut.getFirstPlay).toBeFalsy()
         expect(sut.getSecondPlay).toBeFalsy()
     })
+    test('Should set firstPlay when roll are called for the first time', () => {
+        const sut = new Frame()
+        sut.roll(5)
+        expect(sut.getFirstPlay).toBe(5)
+        expect(sut.getSecondPlay).toBeFalsy()
+    })
     test('Should return 0 when frame doenst has filled with plays', () => {
         const sut = new Frame()
         const result = sut.score()
