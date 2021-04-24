07d24e23b6d85f8cc329442bc99c49c0a9ca5014
Maycon Carlete <maycon.carlete@gmail.com>
2021-04-24 20:03:30 -0300

    feat: ensure Frame getter attributes is undefined when it is created

--- src/models/frame.ts
+++ src/models/frame.ts
@@ -1,6 +1,15 @@
 export class Frame {
     private firstPlay: undefined | number
     private secondPlay: undefined | number
+    
+    get getFirstPlay():undefined | number {
+        return this.firstPlay
+    }
+
+    get getSecondPlay():undefined | number {
+        return this.secondPlay
+    }
+
     score():number{
         return 0
     }
