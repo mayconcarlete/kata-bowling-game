6c4999521b3d141955cf9fcc67476efc3e40633c
Maycon Carlete <maycon.carlete@gmail.com>
2021-04-24 20:03:34 -0300

    test: ensure Frame getter attributes is undefined when it is created

--- tests/frame.test.ts
+++ tests/frame.test.ts
@@ -1,6 +1,11 @@
 import { Frame } from "../src/models/frame"
 
 describe('Frame class', () => {
+    test('Should firstPlay and SecondPlay undefined when Frame are created', () => {
+        const sut = new Frame()
+        expect(sut.getFirstPlay).toBeFalsy()
+        expect(sut.getSecondPlay).toBeFalsy()
+    })
     test('Should return 0 when frame doenst has filled with plays', () => {
         const sut = new Frame()
         const result = sut.score()
