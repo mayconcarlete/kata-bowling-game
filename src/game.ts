8342f818fcac036bae570e7be336c07b2d66d820
Maycon Carlete <maycon.carlete@gmail.com>
2021-04-25 15:38:04 -0300

    feat: ensure frame of Game class are instanciated with correct type frames

--- src/game.ts
+++ src/game.ts
@@ -1,4 +1,14 @@
+import { BonusFrame } from "./models/bonus-frame";
+import { Frame } from "./models/frame";
+
 export class Game {
+    frame: Frame[]|BonusFrame[] = []
+    constructor(){
+        for(let i=0; i<9; i++ ){
+            this.frame[i] = new Frame()
+        }
+        this.frame[9] = new BonusFrame()
+    }
     roll(knocked_pins:number ):void{
         
     }
