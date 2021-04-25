4e418c50c9af9ddd01c2ce5dd55dd4496c67f078
Maycon Carlete <maycon.carlete@gmail.com>
2021-04-24 21:18:49 -0300

    feat: ensure BonusFrame are created with atributes undefined

--- src/models/bonus-frame.ts
+++ src/models/bonus-frame.ts
@@ -2,4 +2,7 @@ import { Frame } from "./frame";
 
 export class BonusFrame extends Frame {
     private thirdPlay: undefined | number
+    get getThirdPlay():undefined | number{
+        return this.thirdPlay
+    }
 }
